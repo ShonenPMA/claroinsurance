1a39fd34b86960ab8a7b10d5d5055d2b5ae6275c
Jhon Ayrton Saavedra Castañeda <ayrtonsc6@gmail.com>
2021-04-30 18:58:53 +0000

    agregando relaciones y test a User, Email

--- app/Models/Email.php
+++ app/Models/Email.php
@@ -11,4 +11,8 @@ class Email extends Model
     use HasFactory, DatesTranslator;
 
     protected $fillable = ['receiver','subject','message','user_id'];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
